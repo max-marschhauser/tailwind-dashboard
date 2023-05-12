f11978417752692bf6a49ba635032e2774ad1cda
Max Marschhauser <max.marschhauser@gmail.com>
2023-05-12 11:03:00 +0200

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
 	return (
 		<>
-			<h1 className="text-3xl font-bold">Hello world!</h1>
+			<h1 className="text-3xl font-bold bg-gray-700 block w-100 text-center first-letter:text-red-600">
+				Hello world!
+			</h1>
 		</>
 	);
 }
